c1af823a943adab57c42c2d0a9c2800301190a0c
HUA HSUAN LIANG <wilson.l20010327@gmail.com>
2024-02-25 19:52:08 +0800

    Add the insert head function
    First, check the input head, malloc a space for the new element,
    after that create a space for the content of the element, if one of them
    fail to create a space then return false. After successfully created,
    add element to the list with the linux kernal api list_add and return
    true.

--- queue.c
+++ queue.c
@@ -37,6 +37,17 @@ void q_free(struct list_head *l)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *element = malloc(sizeof(element_t));
+    char *content = strdup(s);
+    if (!element || !content) {
+        test_free(element);
+        test_free(content);
+        return false;
+    }
+    element->value = content;
+    list_add(&element->list, head);
     return true;
 }
 
